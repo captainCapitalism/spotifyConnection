1add159e274eade292d0855c837f5f0d35dc9ef2
m <michal5bargiel@gmail.com>
2019-04-13 21:40:46 +0200

    Learn program to display album covers

--- routes/index.js
+++ routes/index.js
@@ -3,31 +3,26 @@ const request       = require('request');
 const cors          = require('cors');
 const querystring   = require('querystring');
 const cookieParser  = require('cookie-parser');
-
+const pug           = require('pug');
 
 const {client_id, client_secret, redirect_uri, stateKey} = require('../config/config');
 const app = express();
 
 app.get("/", renderIndex);  
 app.get('/callback', getAlbums);
-function renderIndex(req, res){
-    res.render('index', {title: 'hey', message: 'hello'})
-}
+
+function renderIndex(req, res){ res.render('index'); }
 
 function getAlbums(req, res){
-
     const code = req.query.code || null;
     const state = req.query.state || null;
     const storedState = req.cookies ? req.cookies[stateKey] : null;
   
     if (state === null || state !== storedState) {
-        res.redirect('/#' +
-            querystring.stringify({
-                error: 'state_mismatch'
-            }));
+        sendError(res, 'state_mismatch');
     } else {
         res.clearCookie(stateKey);
-        var authOptions = {
+        const authOptions = {
             url: 'https://accounts.spotify.com/api/token',
             form: {
                 code: code,
@@ -35,40 +30,38 @@ function getAlbums(req, res){
                 grant_type: 'authorization_code'
             },
             headers: {
-                'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+                'Authorization': 'Basic ' + (new Buffer.from(client_id + ':' + client_secret).toString('base64'))
             },
             json: true
         };
-  
-        request.post(authOptions, function(error, response, body) {
-            if (!error && response.statusCode === 200) {
-            var access_token = body.access_token,
-                refresh_token = body.refresh_token;
-    
-            var options = {
+        fetchAlbums(res, authOptions);
+    }
+};
+
+
+function fetchAlbums(res, authOptions){
+    request.post(authOptions, function(error, response, body) {
+        if (!error && response.statusCode === 200) {
+        const access_token = body.access_token;
+        const options = {
                 url: 'https://api.spotify.com/v1/me/albums',
                 headers: {'Authorization': 'Bearer ' + access_token},
                 json: true
-            };
-    
-            // use the access token to access the Spotify Web API
-            request.get(options, function(error, response, body) {
-                console.log(body);
-            });
-            // we can also pass the token to the browser to make requests from there
-            res.redirect('/#' +
-                querystring.stringify({
-                access_token: access_token,
-                refresh_token: refresh_token
-                }));
-            } else {
-            res.redirect('/#' +
-                querystring.stringify({
-                error: 'invalid_token'
-                }));
-            }
+        };
+        request.get(options, function(error, response, body) {
+            let images = body.items.map(
+                album => album.album.images[0].url );
+            res.render('user', {body: images});
         });
-    }
-};
+        } else { sendError(res, 'invalid_token') }
+    });
+}
+
+
+function sendError(res, error){
+    res.redirect('/#' +
+    querystring.stringify({ error: error})
+    );
+}
   
 module.exports = app;
--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-(function() {
+(function renderSite(){
 
     /**
      * Obtains parameters from the hash of the URL
@@ -16,28 +16,21 @@
 
     const params = getHashParams();
 
-    const   access_token = params.access_token,
-            error = params.error;
+    const access_token = params.access_token,
+        error = params.error;
 
     if (error) {alert('There was an error during the authentication')} 
     else {
-      if (access_token) {        
-        const url = 'https://api.spotify.com/v1/me/albums?limit=50';
-        fetchAlbums(access_token, url);
-      } else {
+      if (!access_token) {        
           // render initial screen
           document.getElementById('login').style.display = 'block';
           document.getElementById('loggedin').style.display = 'none';
       }
     }
-  })();
+})();
 
-function fetchAlbums(access_token, url){
-    sendRequest({
-        url: url, 
-        callback: renderNames,
-        method: 'get', 
-        access_token: access_token});
+function getAlbums(){
+    console.log(21);
 }
 
 function sendRequest({url, callback, method, access_token}){
@@ -53,17 +46,17 @@ function sendRequest({url, callback, method, access_token}){
     
 }
 
-function renderNames(response, access_token){
-    response.items.forEach( album => {
-        console.log(album);
-        let dataContainer = document.createElement('div');
-        let jsonAlbum = `<span class='title'>${album.album.name}</span> <span class='artist'>by ${album.album.artists[0].name}</span>`;
-        dataContainer.innerHTML = jsonAlbum;
-        document.body.appendChild(dataContainer);
-    });
-    document.getElementById('login').display = 'block';
-    document.getElementById('loggedin').display = 'none';
-    if(response.next){
-        fetchAlbums(access_token, response.next);
-    }
-}
+// function renderNames(response, access_token){
+//     response.items.forEach( album => {
+//         console.log(album);
+//         let dataContainer = document.createElement('div');
+//         let jsonAlbum = `<span class='title'>${album.album.name}</span> <span class='artist'>by ${album.album.artists[0].name}</span>`;
+//         dataContainer.innerHTML = jsonAlbum;
+//         document.body.appendChild(dataContainer);
+//     });
+//     document.getElementById('login').display = 'block';
+//     document.getElementById('loggedin').display = 'none';
+//     if(response.next){
+//         fetchAlbums(access_token, response.next);
+//     }
+// }
