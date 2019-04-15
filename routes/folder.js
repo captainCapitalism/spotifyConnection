42f269d830b19df9e2be5c9af000774a5e7d22b4
m <michal5bargiel@gmail.com>
2019-04-15 17:38:14 +0200

    Created get route, and route for adding albums to folders

--- routes/folder.js
+++ routes/folder.js
@@ -1,29 +1,36 @@
 const   express       = require('express'),
         fs            = require('fs'),
         app           = express();
+
+app.get("/", fetchFolders);
 app.post("/", createFolder);
+app.put("/", editFolder);
+
+function editFolder(req, res){
+    console.log(req.body);
+    res.send({msg: "put route"});
+}
+
+function fetchFolders(req, res){
+    let file = JSON.parse(fs.readFileSync("folders.json"));
+    res.send({msg: "Fetched folders", file});
+}
 
 function createFolder(req, res){
     if(req.body && req.body.name){
-        const msg = appendJSON("folders.json", req.body.name);
-        if("success" in msg){
-            res.send(msg);
-        } else if("error" in msg){
-            res.send(msg);
-        }
-    } else {
-        res.send({error: "Missing name"});
-    }
+        const result = appendJSON("folders.json", req.body.name);
+        res.send(result);
+    } else {res.send({error: "Missing name"})}
 }
 
 function appendJSON(fileName, data){
     let file = JSON.parse(fs.readFileSync(fileName));
     if(data in file){
-        return {error: "folder with given name already exists"};
+        return {msg:{error: "folder with given name already exists"}, file};
     } else {
         file[data] = {};
         fs.writeFileSync(fileName, JSON.stringify(file));    
-        return {success: "folder created"};
+        return {msg: {success: "folder created"}, file};
     }
 }
 
--- scripts/albums.js
+++ scripts/albums.js
@@ -1,16 +1,78 @@
-function createFolder(){
-    const folderName = document.getElementById('folder-name').value;
+(function fetchFolders(){
     const ajaxOptions = {
         url: "/folder",
-        method: "post",
-        data: {name: folderName},
+        method: "get",
         callback: function(data){
-            console.log(data);
+            createFoldersDatalist(data.file);
+            createMissingFolders(data);
         }
+    }
+    ajax(ajaxOptions);
+})();
+
+function createFoldersDatalist(data){
+    const datalist = document.createElement("datalist");
+    datalist.id = "folder-list";
+    for(key in data){
+        const option = document.createElement("option");
+        option.value = key;
+        datalist.appendChild(option);
+    }
+    document.body.appendChild(datalist);
+    
+}
+
+function createFolder(){
+    const folderName = document.getElementById("folder-name").value;
+    if(folderName){
+        const ajaxOptions = {
+            url: "/folder",
+            method: "post",
+            data: {name: folderName},
+            callback: createMissingFolders
+        }
+        ajax(ajaxOptions);
+    } else {
+        console.log("error: No name provided");
+    }
+}
+
+function createMissingFolders(data){
+    if(data){
+        console.log(data.msg);
+        const folders = data.file;
+        const folderContainer = document.getElementById("folder-container");
+        for(key in folders){
+            const element = document.getElementById("folder-"+key);
+            if(!element){
+                const newElement = document.createElement('div');
+                newElement.id = ('folder-'+key);
+                newElement.classList.add("folder");
+                newElement.innerHTML=key;
+                folderContainer.appendChild(newElement);
+            }
+        }    
+    } else {
+        console.log("Did not receive data");
+    }
+}
+
+function addToFolder(id){
+    const albumContainer = document.getElementById(id);
+    const url = albumContainer.getElementsByTagName('a')[0].href;
+    const folder = albumContainer.getElementsByTagName('input')[0].value;
+    const ajaxOptions = {
+        url: "/folder",
+        method: "put",
+        data: {
+            url: url,
+            folder: folder
+        },
+        callback: callback
     }
     ajax(ajaxOptions);
 }
 
-function myFunction(id){
-    console.log(id);
+function callback(data){
+    console.log(data);
 }
